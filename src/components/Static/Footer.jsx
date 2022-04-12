0dc70f4bf42eb006833ba50a854ad9cf9b6af663
TR/B <103478725+epiklerjs@users.noreply.github.com>
2022-04-12 07:22:50 +0300

    Update Footer.jsx

--- src/components/Static/Footer.jsx
+++ src/components/Static/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = ({ better }) => {
         <div className="lg:flex justify-between w-full">
           <Link href="/">
             <p className="cursor-pointer flex items-center font-semibold text-2xl">
-              <span className="bg-zinc-200 text-xs px-2 py-1 mr-2 rounded-lg">v2.0</span>clqu
+              <span className="bg-zinc-200 text-xs px-2 py-1 mr-2 rounded-lg">v3.0</span>xrqu
               <span style={{ zIndex: 4 }} className="relative text-cIndigo">
                 .
               </span>
@@ -17,7 +17,7 @@ const Footer = ({ better }) => {
           <div className="lg:flex space-x-6 items-center">
             <div className="text-left mt-2 lg:mt-0 lg:text-right">
                 <p>
-                clqu.live &copy; 2020 - {new Date().getFullYear()}, All rights
+                xrqu.live &copy; 2020 - {new Date().getFullYear()}, All rights
                 reserved.
                 </p>
 
